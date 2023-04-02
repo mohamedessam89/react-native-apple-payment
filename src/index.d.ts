92038ec514370875b640b8f8349337e693b0b931
Mohamed Abdelsamei <eng.mohamed3ssam@gmail.com>
2023-04-02 13:37:48 +0200

    feat: add mada to supported networks

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'react-native-apple-payment' {
-  type Network = 'Visa' | 'MasterCard' | 'AmEx';
+  type Network = 'Visa' | 'MasterCard' | 'AmEx' | 'mada';
 
   export type SupportedNetworks = Network[];
 
